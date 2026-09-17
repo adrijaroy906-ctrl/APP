f101353a55b557c9a9afdef66a6e8cde37c3fbbd
Adrija <adrijaroy906@gmail.com>
2026-09-17 13:05:03 +0530

    Week7 Tutorial

---
+++ HospitalManagement/Main.java
@@ -0,0 +1,38 @@
+//Week7_Ques2
+import doctor.Doctor;
+import patient.Patient;
+
+public class Main {
+    public static void main(String[] args) {
+        Doctor d1 = new Doctor(1, "Ravi", "Cardiology", 500);
+        Doctor d2 = new Doctor(2, "Meena", "Dermatology", 400);
+
+        Patient p1 = new Patient(101, "Arun", "Heart", 30);
+        Patient p2 = new Patient(102, "Priya", "Skin", 25);
+        Patient p3 = new Patient(103, "Kiran", "Heart", 40);
+
+        Doctor[] assigned = {d1, d2, d1};
+        Patient[] patients = {p1, p2, p3};
+
+        double total1 = 0, total2 = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\n--- Patient Details ---");
+            patients[i].display();
+
+            System.out.println("Treating Doctor:");
+            assigned[i].display();
+
+            if (assigned[i] == d1)
+                total1 += d1.getConsultationFee();
+            else
+                total2 += d2.getConsultationFee();
+        }
+
+        System.out.println("\nTotal collected by " +
+                d1.getName() + ": " + total1);
+
+        System.out.println("Total collected by " +
+                d2.getName() + ": " + total2);
+    }
+}
